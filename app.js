e8eaf63ff646e9c2fecc8b380e11c0745b92b1c7
jeffrytyn <jeffreytian01@gmail.com>
2020-07-24 18:14:45 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// app.use(function(req, res, next){
+//     res.locals.currentUser = req.user;
+//     res.locals.error = req.flash("error");
+//     res.locals.success = req.flash("success");
+//     next();
+//  });
+
+//Set up default mongoose connection
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+var mongoDB = 'mongodb://127.0.0.1/TravelConnect';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+app.get('/', function(req, res){
+    res.render('index'); //express looks in views directory by default
+});
+
+app.get('/login', function(req, res){
+    res.render('login'); 
+});
+
+app.post('/login', function(req, res){
+    res.send(req.body.username + req.body.password);
+});
+
+app.get('/register', function(req, res){
+    res.render('register'); 
+});
+
+app.post('/register', function(req, res){
+    res.send(req.body.username + req.body.password);
+});
+
+app.listen(port, () => console.log(`Server started on ${port}`));
---
+++ public/scripts.js
@@ -0,0 +1,21 @@
+
+function toggleNavbarBG(){
+    const w = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+    const mainNav = $("#mainNav");
+    if(w >= 768){
+        mainNav.removeClass("bg-dark");
+    }else{
+        if($(".navbar-collapse.collapse").hasClass('show')){
+            mainNav.addClass("bg-dark");
+        }else{
+            mainNav.removeClass("bg-dark");
+        }
+    }
+};
+
+$(".navbar-toggler").click(function(){
+    $("#mainNav").toggleClass("bg-dark");
+});
+window.onresize = toggleNavbarBG;
